8896535b07af85f7aa76ea44dde5049ce5d54037
Balázs Orbán <info@balazsorban.com>
2016-10-07 16:30:49 +0200

    Created shell functions to start up local server faster and automatically change the local and online url

---
+++ setup.bash
@@ -0,0 +1,30 @@
+iplocal=$(ifconfig | grep 'inet addr' | awk '{print $2}' | cut -d ':' -f2 |grep -v 127)
+local(){
+	sleep 4;
+	cd ./.pug/1-ESSENTIALS/;
+	sed -i '3 s=^//==; 2 s=^=//=' vars.jade;
+	cd ../../.jekyll;
+	sed -i '3 s/^#//; 2 s/^/#/' _config.yml;
+	jekyll serve --host 0.0.0.0;
+	cd ..
+}
+
+online(){
+
+	sleep 4;
+	cd ./.pug/1-ESSENTIALS/;
+	sed -i '2 s=^//==; 3 s=^=//=' vars.jade;
+	cd ../../.jekyll;
+	sed -i '2 s/^#//; 3 s/^/#/' _config.yml;
+	jekyll serve --host 0.0.0.0;
+	cd ..
+}
+
+mypage () {
+
+	vivaldi 192.168.1.4:8000 & cd Downloads/Prepros-linux-64&&./Prepros&cd;
+	cd Documents/github/balazsorban44.github.io/;
+	sleep 2;
+	atom .;
+	$1
+}
