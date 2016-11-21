a31c6f092f0b1f75c055d19e9a12088a73fe34ca
Balázs Orbán <info@balazsorban.com>
2016-11-21 20:49:44 +0100

    added node.js and gulp

---
+++ gulpfile.js
@@ -0,0 +1,26 @@
+const gulp = require('gulp');
+const sass = require('gulp-ruby-sass');
+const pug = require('gulp-pug');
+const livereload = require('gulp-livereload');
+
+gulp.task('sass', () =>
+    sass('sass/main.sass')
+        .on('error', sass.logError)
+        .pipe(gulp.dest('assets/css'))
+        .pipe(livereload())
+);
+
+gulp.task('pug', () =>
+  gulp.src('pug/*.pug')
+    .pipe(pug({ pretty: false }))
+    .pipe(gulp.dest('.'))
+    .pipe(livereload())
+
+);
+
+gulp.task('watch', function (){
+  livereload.listen();
+  livereload.reload();
+  gulp.watch('sass/**/*.sass', ['sass']);
+  gulp.watch('pug/**/*.pug', ['pug']);
+});
