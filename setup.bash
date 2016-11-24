d82209de53085eab1440f74644ec3d9808fa7db4
Balázs Orbán <info@balazsorban.com>
2016-11-24 13:47:49 +0100

    Changed mypage() in bash file.

--- setup.bash
+++ setup.bash
@@ -1,30 +1,42 @@
 iplocal=$(ifconfig | grep 'inet addr' | awk '{print $2}' | cut -d ':' -f2 |grep -v 127)
-local(){
-	sleep 4;
-	cd ./.pug/1-ESSENTIALS/;
-	sed -i '3 s=^//==; 2 s=^=//=' vars.jade;
-	cd ../../jekyll;
-	sed -i '3 s/^#//; 2 s/^/#/' _config.yml;
-	jekyll serve --host 0.0.0.0;
-	cd ..
-}
 
-online(){
+mypage() {
+	if [ "$1" = "-l" ]; then
+		sleep 2;
+		cd Documents/github/balazsorban44.github.io/pug/1-ESSENTIALS/;
+    sed -i "2 s/balazsorban.com/$iplocal:8000/g" vars.pug
+		cd ../../jekyll
+    sed -i "2 s/balazsorban.com/$iplocal:8000/g" _config.yml
+		cd ..
+		atom
+		cd ./jekyll
+		jekyll serve --host 0.0.0.0
+		cd ..
 
-	sleep 4;
-	cd ./.pug/1-ESSENTIALS/;
-	sed -i '2 s=^//==; 3 s=^=//=' vars.jade;
-	cd ../../jekyll;
-	sed -i '2 s/^#//; 3 s/^/#/' _config.yml;
-	jekyll serve --host 0.0.0.0;
-	cd ..
-}
-
-mypage () {
-
-	vivaldi 192.168.1.4:8000 & cd Downloads/Prepros-linux-64&&./Prepros&cd;
-	cd Documents/github/balazsorban44.github.io/;
-	sleep 2;
-	atom .;
-	$1
+	elif [ "$1" = "-o" ]; then
+		cd Documents/github/balazsorban44.github.io/pug/1-ESSENTIALS
+		sed -i "2 s=http://.*:8000=http://balazsorban.com=g" vars.pug
+		cd ../../jekyll
+		sed -i "2 s=http://.*:8000=http://balazsorban.com=g" _config.yml
+		cd ..
+		atom
+		cd ./jekyll
+		jekyll serve --host 0.0.0.0
+	elif [ "$1" = "-w" ]; then
+		cd
+		cd Documents/github/balazsorban44.github.io
+    gulp watch
+  elif [ "$1" = "-s" ]; then
+		cd
+		cd Documents/github/balazsorban44.github.io
+    serve
+	elif [ "$1" = "-h" ]; then
+		echo "
+	-l Use -l for local url path.
+	-o Use -o for online url path.
+					"
+	else
+		echo "	Please specify url path.
+	For more help, use -h"
+	fi
 }
