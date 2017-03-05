bdb59b7517c63887663792e915bb3b445b362624
Balázs Orbán <info@balazsorban.com>
2017-03-05 13:40:27 +0100

    Added onload event listener to the JS file.

--- src/js/main.js
+++ src/js/main.js
@@ -1,89 +1,56 @@
-//----------------------- SET AGE -----------------------#
-function getAge(dateString) {
-    let today = new Date(),
-        birthDate = new Date(dateString),
-        age = today.getFullYear() - birthDate.getFullYear(),
-        m = today.getMonth() - birthDate.getMonth()
+window.addEventListener("load", function load(){
+    window.removeEventListener("load", load, false)
+    init()
+},false)
 
-    if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
-        age--
-    }
-    return age
+const init = () => {
+
+  //----------------------- SET AGE -----------------------#
+  function getAge(dateString) {
+      let today = new Date(),
+          birthDate = new Date(dateString),
+          age = today.getFullYear() - birthDate.getFullYear(),
+          m = today.getMonth() - birthDate.getMonth()
+
+      if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
+          age--
+      }
+      return age
+  }
+  document.querySelector('#age').innerText = getAge('1994.12.17')
+
+  //----------------------- SET AGE -----------------------#
+
+
+  const intro = document.querySelector('#intro'),
+        meText = document.querySelector('#me-text'),
+        open = document.querySelector('.open'),
+        close = document.querySelector('.close'),
+        card = document.querySelector('#card'),
+        landing = document.querySelector('#landing'),
+        iH = window.innerHeight
+
+
+  open.addEventListener('click', () => {
+    // window.history.pushState('more','more','/more')
+    meText.classList.remove('rotated-left')
+    intro.classList.add('rotated-right')
+  })
+
+  close.addEventListener('click', () => {
+    // window.history.back()
+    intro.classList.remove('rotated-right')
+    meText.classList.add('rotated-left')
+  })
+
+  if (window.matchMedia('(hover:hover)').matches) {
+    document.onmousemove = handleMouseMove
+  }
+
+  function handleMouseMove(e){
+    let x = e.clientX,
+        y = e.clientY
+    card.style.margin = `${iH/20 + y/20}px 0`
+    landing.style.minWidth = `${70+ x/100}vw`
+  }
 }
-document.querySelector('#age').innerText = getAge('1994.12.17')
-
-//----------------------- SET AGE -----------------------#
-
-
-const intro = document.querySelector('#intro'),
-      meText = document.querySelector('#me-text'),
-      open = document.querySelector('.open'),
-      close = document.querySelector('.close'),
-      card = document.querySelector('#card'),
-      landing = document.querySelector('#landing'),
-      iH = window.innerHeight
-
-
-open.addEventListener('click', () => {
-  // window.history.pushState('more','more','/more')
-  meText.classList.remove('rotated-left')
-  intro.classList.add('rotated-right')
-})
-
-close.addEventListener('click', () => {
-  // window.history.back()
-  intro.classList.remove('rotated-right')
-  meText.classList.add('rotated-left')
-})
-
-if (window.matchMedia('(hover:hover)').matches) {
-  document.onmousemove = handleMouseMove
-}
-
-function handleMouseMove(e){
-  let x = e.clientX,
-      y = e.clientY
-  card.style.margin = `${iH/20 + y/20}px 0`
-  landing.style.minWidth = `${70+ x/100}vw`
-}
-
-// const strength = 50
-// let shouldMove = false
-//
-// document.querySelector('body').addEventListener('click', () => {
-//   if (!shouldMove) {
-//     shouldMove = true
-//   }
-//   else {
-//     shouldMove = false
-//   }
-// })
-// document.onmousemove = handleMouseMove
-//
-// function handleMouseMove(e){
-//   if (shouldMove) {
-//     let x = e.clientX,
-//         y = e.clientY
-//
-//     if (y<iH/2) {
-//       if (x<iW/2) {
-//         card.transform =`
-//           rotateX(${(y-iH/2)/strength}deg) rotateY(${(-(iW-x))/strength}deg)`
-//       }
-//       else {
-//         card.transform =`
-//           rotateX(${(y-iH/2)/strength}deg) rotateY(${x/strength}deg)`
-//       }
-//     }
-//     else {
-//       if (x<iW/2) {
-//         card.transform =`
-//           rotateX(${y/strength}deg) rotateY(${((x-iW)/2)/strength}deg)`
-//       }
-//       else {
-//         card.transform =`
-//           rotateX(${y/strength}deg) rotateY(${x/strength}deg)`
-//       }
-//     }
-//   }
-// }
