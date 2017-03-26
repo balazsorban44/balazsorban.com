d2d4da6f2e30a2b180a6488227c5b26fdf5a67cd
Balázs Orbán <info@balazsorban.com>
2017-03-27 00:57:31 +0200

    Rewrote and modularized main.js

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,6 @@
+const tag = document.createElement("script")
+tag.src = "https://unpkg.com/@reactivex/rxjs@latest/dist/global/Rx.min.js"
+document.getElementsByTagName("head")[0].appendChild(tag)
 window.addEventListener("load", function load(){
     window.removeEventListener("load", load, false)
     init()
@@ -5,52 +8,134 @@ window.addEventListener("load", function load(){
 
 const init = () => {
 
-  //----------------------- SET AGE -----------------------#
-  function getAge(dateString) {
+const iW = window.innerWidth
+
+  // LANDING PAGE
+if (document.querySelector('.landing') !== null) {
+if (iW >= 1024) {
+  const docElm = document.documentElement,
+  cardElm = document.querySelector('#card'),
+  { clientWidth, clientHeight } = docElm,
+  mouseMove$ = Rx.Observable
+  .fromEvent(docElm, 'mousemove')
+  .map(event => ({ x: event.clientX, y: event.clientY })),
+
+  touchMove$ = Rx.Observable
+  .fromEvent(docElm, 'touchmove')
+  .map(event => ({
+    x: event.touches[0].clientX,
+    y: event.touches[0].clientY
+  })),
+
+  lerp = (start, end) => {
+    const dx = end.x - start.x,
+    dy = end.y - start.y
+
+    return {
+      x: start.x + dx * 0.8,
+      y: start.y + dy * 0.8,
+    };
+  }
+
+  const move$ = Rx.Observable.merge(mouseMove$, touchMove$),
+  animationFrame$ = Rx.Observable.interval(0, Rx.Scheduler.animationFrame),
+
+  smoothMove$ = animationFrame$
+  .withLatestFrom(move$, (tick, move) => move)
+  .scan(lerp);
+
+  smoothMove$.subscribe(pos => {
+    const rotX = (pos.y / clientHeight * -50) + 25,
+    rotY = (pos.x / clientWidth * 50) - 25
+
+    cardElm.style.cssText = `transform: rotateX(${rotX}deg) rotateY(${rotY}deg);`
+  })
+}
+
+
+
+  const intro = document.querySelector('#intro'),
+        meText = document.querySelector('#me-text'),
+        name = document.querySelector('#name')
+
+  //----------------------- SET AGE -----------------------//
+  let getAge = (dateString) => {
       let today = new Date(),
           birthDate = new Date(dateString),
           age = today.getFullYear() - birthDate.getFullYear(),
           m = today.getMonth() - birthDate.getMonth()
 
-      if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
-          age--
-      }
+      if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {age--}
       return age
   }
+
   document.querySelector('#age').innerText = getAge('1994.12.17')
 
-  //----------------------- SET AGE -----------------------#
+  //----------------------- SET AGE -----------------------//
 
 
-  const intro = document.querySelector('#intro'),
-        meText = document.querySelector('#me-text'),
-        open = document.querySelector('.open'),
-        close = document.querySelector('.close'),
-        card = document.querySelector('#card'),
-        landing = document.querySelector('#landing'),
-        iH = window.innerHeight
-
-
-  open.addEventListener('click', () => {
+  intro.addEventListener('click', () => {
     // window.history.pushState('more','more','/more')
     meText.classList.remove('rotated-left')
     intro.classList.add('rotated-right')
+    if (iW <= 768) {
+      name.style.display = 'none'
+    }
   })
 
-  close.addEventListener('click', () => {
+  meText.addEventListener('click', () => {
     // window.history.back()
     intro.classList.remove('rotated-right')
     meText.classList.add('rotated-left')
+    if (iW <= 768) {
+      name.style.display = 'flex'
+    }
   })
 
-  if (window.matchMedia('(hover:hover)').matches) {
-    document.onmousemove = handleMouseMove
+  // let handleMouseMove = (e) => {
+  //   let x = e.clientX,
+  //   y = e.clientY
+  //   card.style.margin = `${iH/20 + y/20}px 0`
+  //   // landing.style.minWidth = `${55+ x/100}vw`
+  // }
+  // if (window.matchMedia('(hover:hover)').matches) {
+  //   document.onmousemove = handleMouseMove
+  // }
+
+
+}
+
+// PHOTO POSTS
+if (document.querySelector('.photo') !== null) {
+  const backButton = document.querySelector('#post-photo footer'),
+        contentImg = document.querySelector('#content-img img'),
+        bgImg = document.querySelector('#bg-img img')
+
+  if (iW < 768) {
+    backButton.style.top = `${contentImg.height}px`
   }
 
-  function handleMouseMove(e){
-    let x = e.clientX,
-        y = e.clientY
-    card.style.margin = `${iH/20 + y/20}px 0`
-    landing.style.minWidth = `${70+ x/100}vw`
+  bgImg.setAttribute("src", contentImg.getAttribute("src"))
+
+}
+
+if (document.querySelectorAll('header').length !== 0) {
+  const menuElements = document.querySelectorAll('a span')
+
+  if (window.location.href.includes('photo')) {
+    menuElements[1].style.color= 'rgb(173, 47, 0)'
   }
+  else if (window.location.href.includes('code')) {
+    menuElements[0].style.color= 'rgb(173, 47, 0)'
+  }
+
+}
+
+  // menu = document.querySelector('.hamburger-menu'),
+  // menu.addEventListener('click', () => {
+  //   menu.classList.toggle('grow')
+  // })
+  //
+
+
 }
