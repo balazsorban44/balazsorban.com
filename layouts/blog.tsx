459704b4b7f230440bbcba54caf9b83d22f989c8
Balázs Orbán <info@balazsorban.com>
2021-11-24 02:47:13 +0100

    tweak blog layout

--- layouts/blog.tsx
+++ layouts/blog.tsx
@@ -1,18 +1,10 @@
 import Image from "next/image"
 import { parseISO, format } from "date-fns"
-
 import Container from "components/container"
-import ViewCounter from "components/view-counter"
+// import ViewCounter from "components/view-counter"
+
 import type { PropsWithChildren } from "react"
 import type { Blog } from ".contentlayer/types"
-import { Link } from "components/link"
-
-const editUrl = (slug) =>
-  `https://github.com/balazsorban44/balazsorban.com/edit/main/data/blog/${slug}.mdx`
-const discussUrl = (slug) =>
-  `https://mobile.twitter.com/search?q=${encodeURIComponent(
-    `https://balazsorban.com/blog/${slug}`
-  )}`
 
 export default function BlogLayout(props: PropsWithChildren<{ post: Blog }>) {
   const { children, post } = props
@@ -44,8 +36,8 @@ export default function BlogLayout(props: PropsWithChildren<{ post: Blog }>) {
           </div>
           <p className="mt-2 text-sm min-w-32 md:mt-0">
             {post.readingTime.text}
-            {` • `}
-            <ViewCounter slug={post.slug} />
+            {/* {` • `}
+            <ViewCounter slug={post.slug} /> */}
           </p>
         </div>
         <div className="w-full mt-4 prose max-w-none">{children}</div>
